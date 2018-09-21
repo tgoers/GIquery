28df61630002d8594c8fc5015c0b36eb6cfd6663
Tristan Goers <tgoers@alaska.edu>
2018-09-20 20:21:07 -0800

    Home page HTML

--- tablesearch/models.py
+++ tablesearch/models.py
@@ -9,4 +9,11 @@ class Tables(models.Model):
     cur = conn.cursor()
     # """SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_SCHEMA='public'"""
     cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_SCHEMA='public'")
-    rows = cur.fetchall()
+    all_tables = cur.fetchall()
+
+class DataType(models.Model):
+    conn = psycopg2.connect("dbname='giquery-data' user='postgres' host='localhost' password='admin'")
+    cur = conn.cursor()
+    # """SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_SCHEMA='public'"""
+    cur.execute("SELECT * FROM geometry_columns WHERE f_table_schema = 'public' AND f_geometry_column = 'geom'")
+    data_type = cur.fetchall()
